28c825ad29d3886cb88ae44c2436a1aa2dec0f4e
Va-silly <45421115+Vasilyko@users.noreply.github.com>
2019-11-15 10:59:39 +0100

    Add files via upload

---
+++ streetfocus-app/src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'news',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../news/news.module').then(m => m.NewsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'contact',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../contact/contact.module').then(m => m.ContactPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/news',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/news',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
